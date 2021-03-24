0d1ea6262fc04e2a488e58a5225543e6d623c544
babartos <mbartos@smu.edu>
2021-03-23 19:18:34 -0500

    test commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Login } from "./login";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Login />, rootElement)

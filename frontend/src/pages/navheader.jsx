5257ff999ea299400b3814c04973b9971d9da89e
babartos <mbartos@smu.edu>
2021-04-05 10:58:50 -0500

    nav bar addition

--- frontend/src/pages/navheader.jsx
+++ frontend/src/pages/navheader.jsx
@@ -3,11 +3,12 @@ import { Link, Redirect } from 'react-router-dom';
 
 function NavHeader(props) {
         return(
-            <h1 className="bg-primary row p-3 pl-4">MyConstruction
-                <div className="col btn-col">
-                    <div className="">
-                        <Link type="button" className="btn btn-primary" to="/login">Login/Signup</Link>
-                    </div>
+            <h1 className="h1 bg-primary row p-3 pl-4">MyConstruction
+                <div className="pl-2">
+                    <Link type="button" className="btn btn-primary" to="/">Home</Link>
+                </div>
+                <div className="pl-2">
+                    <Link type="button" className="btn btn-primary" to="/login">Login/Signup</Link>
                 </div>
             </h1>
 

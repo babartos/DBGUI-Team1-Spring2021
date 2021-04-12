4b56a86f171cd8390a6f63ee88f8a30ccca5fcfb
babartos <mbartos@smu.edu>
2021-04-12 09:03:50 -0500

    changed sign up fields to reflect backend progress

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import { Link, Redirect } from 'react-router-dom';
 
 export class Signup extends React.Component {
+    accountTypes = ['Average Joe','Professional','Admin'];
+
     state = {
       username: "",
-      password: ""
+      password: "",
+      email: "",
+      accountType: "",
+      firstName: "",
+      lastName: ""
     };
 
     render() {
@@ -12,6 +18,20 @@ export class Signup extends React.Component {
         <form className="border p-3 m-4">
             <header className="display-3 pb-2">Signup</header>
             <p className="mt-1">Already have an account? <Link to='/login'>Login</Link></p> 
+
+            <div class="form-group row">
+                <label for="inputfirstName3" class="col-sm-2 col-form-label">First Name</label>
+                <div class="col-sm-10">
+                    <input type="username" class="form-control" id="inputfirstName3" placeholder="First Name" onChange={(myEvent) => this.setState({ firstName: myEvent.target.value })}/>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputLastName3" class="col-sm-2 col-form-label">Last Name</label>
+                <div class="col-sm-10">
+                    <input type="username" class="form-control" id="inputLastName3" placeholder="Last Name" onChange={(myEvent) => this.setState({ lastName: myEvent.target.value })}/>
+                </div>
+            </div>
+
 
             <div class="form-group row">
                 <label for="inputUsername3" class="col-sm-2 col-form-label">Username</label>
@@ -25,41 +45,33 @@ export class Signup extends React.Component {
                 <input type="password" class="form-control" id="inputPassword3" placeholder="Password" onChange={(myEvent) => this.setState({ password: myEvent.target.value })}/>
                 </div>
             </div>
-            <fieldset class="form-group">
-                <div class="row">
-                    <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
-                    <div class="col-sm-10">
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked/>
-                            <label class="form-check-label" for="gridRadios1">
-                                First radio
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2"/>
-                            <label class="form-check-label" for="gridRadios2">
-                                Second radio
-                            </label>
-                        </div>
-                        <div class="form-check disabled">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled/>
-                            <label class="form-check-label" for="gridRadios3">
-                                Third disabled radio
-                            </label>
-                        </div>
-                    </div>
-                </div>
-            </fieldset>
             <div class="form-group row">
-                <div class="col-sm-2">Checkbox</div>
-                    <div class="col-sm-10">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="gridCheck1"/>
-                            <label class="form-check-label" for="gridCheck1">
-                            Example checkbox
-                            </label>
-                        </div>
-                    </div>
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Email" onChange={(myEvent) => this.setState({ email: myEvent.target.value })}/>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputAboutme3" class="col-sm-2 col-form-label">About me description</label>
+                <div class="col-sm-10">
+                <textarea rows="6" type="aboutme" class="form-control" id="inputaboutme3" placeholder="About Me" onChange={(myEvent) => this.setState({ email: myEvent.target.value })}/>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputAccountType3" class="col-sm-2 col-form-label">Account Type </label>
+                <div class="col-sm-10">
+                    <select
+                        id="accountType"
+                        className="form-control"
+                        value={this.state.accountType}
+                        onChange={e => this.setState({ accountType: (e.target.value)})}>
+                        <option></option>
+                        {
+                            this.accountTypes.map((x, i) => <option key={ i }>{ x }</option>)
+
+                        }
+                    </select>
+                </div>    
             </div>
             <div class="form-group row">
                 <div class="col-sm-10">

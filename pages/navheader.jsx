c668421633133014c09d67f10f3997ad473df730
Tim-Desmond <48928235+Tim-Desmond@users.noreply.github.com>
2021-04-14 15:32:55 -0700

    Add files via upload

---
+++ pages/navheader.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link, Redirect } from 'react-router-dom';
+
+function NavHeader(props) {
+        return(
+            <h1 className="h1 bg-primary row p-3 pl-4 pb-4">MyConstruction
+                <div className="pl-2">
+                    <Link type="button" className="btn btn-primary" to="/">Home</Link>
+                </div>
+                <div className="pl-2">
+                    <Link type="button" className="btn btn-primary" to="/login">Login/Signup</Link>
+                </div>
+            </h1>
+
+    ); 
+
+}
+
+export default NavHeader;
